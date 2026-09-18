4d4f5f76bf7cfea77f71af48fba5a79e1438605d
Buddhadev Maity <buddhadev09101996@gmail.com>
2026-09-18 19:58:26 +0530

    new commit

---
+++ tests/Practice/04_Test_Wingify.spec.ts
@@ -0,0 +1,7 @@
+import {test, expect} from 'playwright/test';
+
+test("Test wingify loin", async function({page}){
+    await page.goto('https://app.wingify.com/#/login');
+
+    await page.locator('//input[@id="login-username"]').fill('buddhadev@gmail');
+})
